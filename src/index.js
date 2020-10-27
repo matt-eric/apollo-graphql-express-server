01c62b5944489d6983edebb3714edee75ef337c4
matt-eric <mf9646a@alumni.american.edu>
2020-10-26 20:19:56 -0400

    Add Express server middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
 const app = express();
 
 app.use(cors());
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
