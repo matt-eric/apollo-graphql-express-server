036ee7a6e831414a58887f6f98904a9ebe6fa010
matt-eric <mf9646a@alumni.american.edu>
2020-10-28 02:48:56 -0400

    Sequelize models

--- src/models/index.js
+++ src/models/index.js
@@ -1,30 +1,12 @@
-let users = {
-  1: {
-    id: '1',
-    username: 'Matt Eric',
-    effectIds: [1],
-  },
-  2: {
-    id: '2',
-    username: 'User Two',
-    effectIds: [2],
-  },
-};
+import Sequelize from 'sequelize';
 
-let effects = {
-  1: {
-    id: '1',
-    type: 'Waveform',
-    userId: '1',
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: 'postgres',
   },
-  2: {
-    id: '2',
-    type: 'Grid Cells',
-    userId: '2',
-  },
-};
+);
 
-export default {
-  users,
-  effects,
-};
+export { sequelize };
