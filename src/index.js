590af3808258e865257dc545f5afbc1efcdee7bb
matt-eric <mf9646a@alumni.american.edu>
2020-10-27 23:44:23 -0400

    Refactor schema into new directory

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 
 import models from './models';
 import resolvers from './resolvers';
-
+import schema from './schema'
 
 const app = express();
 
 app.use(cors());
-
-const schema = gql`
-  type Query {
-    users: [User!]
-    user(id: ID!): User
-    me: User
-
-    effects: [Effect!]!
-    effect(id: ID!): Effect!
-  }
-
-  type Mutation {
-    createEffect(type: String!): Effect!
-    deleteEffect(id: ID!): Boolean!
-  }
-
-  type User {
-    id: ID!
-    username: String!
-    effects: [Effect!]
-  }
-
-  type Effect {
-    id: ID!
-    type: String!
-    user: User!
-  }
-`;
 
 const server = new ApolloServer({
   typeDefs: schema,
