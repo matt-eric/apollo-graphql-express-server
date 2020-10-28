d0755f82241dfae314930c861747503c05b33d46
matt-eric <mf9646a@alumni.american.edu>
2020-10-27 23:29:09 -0400

    Add models directory

---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'Matt Eric',
+    effectIds: [1],
+  },
+  2: {
+    id: '2',
+    username: 'User Two',
+    effectIds: [2],
+  },
+};
+
+let effects = {
+  1: {
+    id: '1',
+    type: 'Waveform',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    type: 'Grid Cells',
+    userId: '2',
+  },
+};
+
+export default {
+  users,
+  effects,
+};
