dcba74b82d147a870e1ab7460819718efaf89db7
matt-eric <mf9646a@alumni.american.edu>
2020-10-27 23:48:18 -0400

    Modularize user schema

---
+++ src/schema/user.js
@@ -0,0 +1,15 @@
+import { gql } from 'apollo-server-express';
+
+export default gql`
+  type Query {
+    users: [User!]
+    user(id: ID!): User
+    me: User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    effects: [Effect!]
+  }
+`;
