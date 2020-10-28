4cc46b6bef7c63d16517114d1db8ea14546effb6
matt-eric <mf9646a@alumni.american.edu>
2020-10-28 03:36:24 -0400

    Add eraseDatabaseOnSync

--- src/index.js
+++ src/index.js
@@ -22,7 +22,13 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-sequelize.sync().then(async () => {
+const eraseDatabaseOnSync = true;
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+
   app.listen({ port: 8000 }, () => {
     console.log('Apollo Server on http://localhost:8000/graphql');
   });
