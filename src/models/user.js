2dcf82d73e6034917a478cd01d87e7954088f305
matt-eric <mf9646a@alumni.american.edu>
2020-10-28 02:47:00 -0400

    Add user model

---
+++ src/models/user.js
@@ -0,0 +1,15 @@
+const user = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    username: {
+      type: DataTypes.STRING,
+    },
+  });
+
+  User.associate = models => {
+    User.hasMany(models.Effect, { onDelete: 'CASCADE' });
+  };
+
+  return User;
+};
+
+export default user;
