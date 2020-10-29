f738d0ede38239cd0d8d775bf076fb62ba23a532
matt-eric <mf9646a@alumni.american.edu>
2020-10-28 23:54:59 -0400

    Update schema with module values currently used in client-side seed JSON

--- src/graphQL/schema/effect.js
+++ src/graphQL/schema/effect.js
@@ -14,6 +14,28 @@ export default gql`
   type Effect {
     id: ID!
     type: String!
+    displayName: String!
+    bypass: Boolean!
+    settings: [ParameterSpecifications]
     user: User!
+  }
+
+  type ParamInt {
+    value: Int
+  }
+
+  type ParamFloat {
+    value: Float
+  }
+
+  union ParamValue = ParamInt | ParamFloat
+
+  type ParameterSpecifications {
+    type: String!
+    displayName: String!
+    value: ParamValue
+    max: ParamValue
+    min: ParamValue
+    step: ParamValue
   }
 `;
