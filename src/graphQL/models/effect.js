4ca0916f43d4eb91a02adeecdc724cb105408435
matt-eric <mf9646a@alumni.american.edu>
2020-10-29 00:32:50 -0400

    Update effect model to match schema

--- src/graphQL/models/effect.js
+++ src/graphQL/models/effect.js
@@ -6,6 +6,54 @@ const effectSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    displayName: {
+      type: String,
+      required: true,
+    },
+    pointShape: {
+      type: String,
+      required: false,
+    },
+    bypass: {
+      type: Boolean,
+      required: true,
+    },
+    settings: [
+      new mongoose.Schema({
+        type: {
+          type: String,
+          required: true,
+        },
+        displayName: {
+          type: String,
+          required: true,
+        },
+        value: {
+          value: {
+            type: Number,
+            required: true,
+          }
+        },
+        max: {
+          value: {
+            type: Number,
+            required: true,
+          }
+        },
+        min: {
+          value: {
+            type: Number,
+            required: true,
+          }
+        },
+        step: {
+          value: {
+            type: Number,
+            required: true,
+          }
+        },
+      })
+    ],
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   },
   {
